6289ad46270f69d1a80d00383a7657366a62b508
dong <dong@dongdembp.lan>
2014-09-10 12:45:39 +0800

    nosting

--- Welian/Class/Login(登陆)/Controller/LogInController.m
+++ Welian/Class/Login(登陆)/Controller/LogInController.m
@@ -14,14 +14,6 @@
 
 @implementation LogInController
 
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
-{
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
-        // Custom initialization
-    }
-    return self;
-}
 
 - (void)viewDidLoad
 {
