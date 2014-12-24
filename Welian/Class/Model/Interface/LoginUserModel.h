54baf36c696441baab7d3137bd34a6abffd7a140
dong <dong@dongdeMacBook-Pro.local>
2014-12-24 17:10:26 +0800

    ...

---
+++ Welian/Class/Model/Interface/LoginUserModel.h
@@ -0,0 +1,13 @@
+//
+//  LoginUserModel.h
+//  Welian
+//
+//  Created by dong on 14/12/24.
+//  Copyright (c) 2014年 chuansongmen. All rights reserved.
+//
+
+#import "IFBase.h"
+
+@interface LoginUserModel : IFBase
+
+@end
