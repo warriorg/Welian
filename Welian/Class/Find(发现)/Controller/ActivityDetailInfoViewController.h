fa281f3772d4fd07f9fc6521ef8ecf9a66a8bcc3
liuwu <liuwugui@gmail.com>
2015-02-07 17:26:11 +0800

    活动列表页面添加

---
+++ Welian/Class/Find(发现)/Controller/ActivityDetailInfoViewController.h
@@ -0,0 +1,13 @@
+//
+//  ActivityDetailInfoViewController.h
+//  Welian
+//
+//  Created by weLian on 15/2/7.
+//  Copyright (c) 2015年 chuansongmen. All rights reserved.
+//
+
+#import "BasicViewController.h"
+
+@interface ActivityDetailInfoViewController : BasicViewController
+
+@end
