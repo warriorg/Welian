7ca72658513b790f70ef6504f57eeb20820dec10
liuwu <liuwugui@gmail.com>
2015-02-14 18:39:43 +0800

    是否认证投资人

--- Welian/Class/Find(发现)/Controller/ProjectDetailsViewController.m
+++ Welian/Class/Find(发现)/Controller/ProjectDetailsViewController.m
@@ -802,8 +802,8 @@ static NSString *noCommentCell = @"NoCommentCell";
 - (void)showProjectInfo
 {
     LogInUser *loginUser = [LogInUser getCurrentLoginUser];
-    //认证投资人或者自己创建的项目可以查看融资信息
-    if (loginUser.investorauth.boolValue || loginUser.uid.integerValue == _projectDetailInfo.rsProjectUser.uid.integerValue) {
+    //认证投资人或者自己创建的项目可以查看融资信息  /**  投资者认证  0 默认状态  1  认证成功  -2 正在审核  -1 认证失败 */
+    if (loginUser.investorauth.integerValue == 1 || loginUser.uid.integerValue == _projectDetailInfo.rsProjectUser.uid.integerValue) {
         [self openProjectDetailInfoView];
     }else{
         [UIAlertView bk_showAlertViewWithTitle:nil
