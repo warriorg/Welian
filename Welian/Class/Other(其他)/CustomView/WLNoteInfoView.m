de619a84ff45dc92e72140efb1df8b029ada5c37
liuwu <liuwugui@gmail.com>
2015-04-01 16:44:05 +0800

    系统功能完善

--- Welian/Class/Other(其他)/CustomView/WLNoteInfoView.m
+++ Welian/Class/Other(其他)/CustomView/WLNoteInfoView.m
@@ -12,6 +12,7 @@
 
 @property (assign,nonatomic) UIActivityIndicatorView *activityView;//加载
 @property (assign,nonatomic) UILabel *noteLabel;
+@property (assign,nonatomic) UIButton *reloadBtn;
 
 @end
 
@@ -26,12 +27,24 @@
     return self;
 }
 
+- (void)setLoadFailed:(BOOL)loadFailed
+{
+    [super willChangeValueForKey:@"loadFailed"];
+    _loadFailed = loadFailed;
+    [super didChangeValueForKey:@"loadFailed"];
+    _reloadBtn.hidden = !_loadFailed;
+    _noteLabel.hidden = _loadFailed;
+    _activityView.hidden = _loadFailed;
+}
+
 - (void)setIsLoaded:(BOOL)isLoaded
 {
     [super willChangeValueForKey:@"isLoaded"];
     _isLoaded = isLoaded;
     [super didChangeValueForKey:@"isLoaded"];
 //    _activityView.hidden = _isLoaded;
+    _reloadBtn.hidden = YES;
+    _noteLabel.hidden = NO;
     if (_isLoaded) {
         [_activityView stopAnimating];
     }else{
@@ -44,6 +57,8 @@
     [super willChangeValueForKey:@"noteInfo"];
     _noteInfo = noteInfo;
     [super didChangeValueForKey:@"noteInfo"];
+    _reloadBtn.hidden = YES;
+    _noteLabel.hidden = NO;
     _noteLabel.text = _noteInfo;
     
     [self setNeedsLayout];
@@ -66,6 +81,10 @@
         _noteLabel.left = _noteLabel.left + 7;
     }
     _activityView.right = _noteLabel.left - 15.f;
+    
+    _reloadBtn.size = CGSizeMake(self.width - 40.f, 40.f);
+    _reloadBtn.centerX = self.width / 2.f;
+    _reloadBtn.centerY = self.height / 2.f;
 }
 
 #pragma mark - Private
@@ -82,6 +101,27 @@
     noteLabel.font = [UIFont systemFontOfSize:14.f];
     [self addSubview:noteLabel];
     self.noteLabel = noteLabel;
+    
+    UIButton *reloadBtn = [UIButton buttonWithType:UIButtonTypeCustom];
+    reloadBtn.backgroundColor = [UIColor clearColor];
+    reloadBtn.titleLabel.font = [UIFont systemFontOfSize:16.f];
+    [reloadBtn setTitle:@"重新加载" forState:UIControlStateNormal];
+    [reloadBtn setTitleColor:kTitleTextColor forState:UIControlStateNormal];
+    [reloadBtn addTarget:self action:@selector(reloadBtnClicked:) forControlEvents:UIControlEventTouchUpInside];
+    reloadBtn.hidden = YES;
+    reloadBtn.layer.borderColor = [UIColor lightGrayColor].CGColor;
+    reloadBtn.layer.borderWidth = 0.8f;
+    reloadBtn.layer.cornerRadius = 5.f;
+    reloadBtn.layer.masksToBounds = YES;
+    [self addSubview:reloadBtn];
+    self.reloadBtn = reloadBtn;
+}
+
+- (void)reloadBtnClicked:(UIButton *)sender
+{
+    if (_reloadBlock && _loadFailed) {
+        _reloadBlock();
+    }
 }
 
 @end
