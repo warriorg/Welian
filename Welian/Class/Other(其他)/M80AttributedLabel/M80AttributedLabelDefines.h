7331154f58fb082e70e10e4e551f3f2f261a9acb
zhangyandong <zhangyandong178@icloud.com>
2015-04-01 15:27:22 +0800

    键盘处理

--- Welian/Class/Other(其他)/M80AttributedLabel/M80AttributedLabelDefines.h
+++ Welian/Class/Other(其他)/M80AttributedLabel/M80AttributedLabelDefines.h
@@ -27,7 +27,7 @@ typedef enum
 typedef NSArray *(^M80CustomDetectLinkBlock)(NSString *text);
 
 //如果文本长度小于这个值,直接在UI线程做Link检测,否则都dispatch到共享线程
-#define M80MinAsyncDetectLinkLength 50
+#define M80MinAsyncDetectLinkLength 10
 
 #define M80IOS7 ([[[UIDevice currentDevice] systemVersion] doubleValue] >= 7.0)
 
