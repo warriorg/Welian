940797515e2ba0a24f4690f5b14bc23111e88527
liuwu <liuwugui@gmail.com>
2015-04-16 16:49:56 +0800

    手机号码验证正则更新

--- Welian/Class/Utils(操作)/Category/NSString+Extend.m
+++ Welian/Class/Utils(操作)/Category/NSString+Extend.m
@@ -77,22 +77,22 @@
      * 电信：133,1349,153,180,189
      */
 //    NSString * MOBILE = @"[\\d\\+]*1(3[0-9]|5[0-35-9]|8[025-9])\\d{8}$";
-    NSString * MOBILE = @"[\\d\\+]*1([357][0-9]|5[0-35-9]|8[025-9])\\d{8}$";
+    NSString * MOBILE = @"[\\d\\+]*1([37][0-9]|5[0-35-9]|8[025-9])\\d{8}$";
     /**
      * 中国移动：China Mobile
      * 134[0-8],135,136,137,138,139,150,151,157,158,159,182,187,188
      */
-    //    NSString * CM = @"1(34[0-8]|(3[5-9]|5[017-9]|8[278])\\d)\\d{7}$";
+    NSString * CM = @"1(34[0-8]|(3[5-9]|5[017-9]|8[278])\\d)\\d{7}$";
     /**
      * 中国联通：China Unicom
      * 130,131,132,152,155,156,185,186
      */
-    //    NSString * CU = @"1(3[0-2]|5[256]|8[56])\\d{8}$";
+    NSString * CU = @"1(3[0-2]|5[256]|8[56])\\d{8}$";
     /**
      * 中国电信：China Telecom
      * 133,1349,153,180,189
      */
-    //    NSString * CT = @"1((33|53|8[09])[0-9]|349)\\d{7}$";
+    NSString * CT = @"1((33|53|8[09])[0-9]|349)\\d{7}$";
     /**
      * 大陆地区固话及小灵通
      * 区号：010,020,021,022,023,024,025,027,028,029
@@ -101,8 +101,14 @@
     // NSString * PHS = @"^0(10|2[0-5789]|\\d{3})\\d{7,8}$";
     
     NSPredicate *regextestmobile = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", MOBILE];
+    NSPredicate *regextestcm = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", CM];
+    NSPredicate *regextestcu = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", CU];
+    NSPredicate *regextestct = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", CT];
     
-    if ([regextestmobile evaluateWithObject:self] == YES)
+    if ([regextestmobile evaluateWithObject:self] == YES
+        || ([regextestcm evaluateWithObject:self] == YES)
+        || ([regextestct evaluateWithObject:self] == YES)
+        || ([regextestcu evaluateWithObject:self] == YES))
     {
         return YES;
     }
